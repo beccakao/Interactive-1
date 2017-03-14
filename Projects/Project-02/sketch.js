e9898cabd78610c1fad98619c723851527b94f89
beccakao <rkao@cca.edu>
2017-03-14 14:26:20 -0700

    Trial

---
+++ Projects/Project-02/sketch.js
@@ -0,0 +1,103 @@
+var capture;
+var value1=0;
+var value2=255;
+
+function setup() {
+	createCanvas(1500,735);
+	capture = createCapture(VIDEO);
+  	capture.size(320,240);
+}
+
+function draw() {
+	background(255);
+	frameRate(4);
+	image(capture, 0, 0, 320, 240);
+	filter('INVERT');
+
+//Start S
+//top horizontal
+	for(var i = 0; i<200; i+=8){
+		fill(value1);
+		noStroke();
+		ellipse(100+i*1.25,100,45,45);
+			if(value1 == 0){
+				value1 =255;
+			} else {
+				value1=0;
+			}	
+	}
+
+//left vertical
+	for(var i = 0; i<120; i+=9){
+		fill(value1);
+		noStroke();
+		ellipse(100,107+i,60,60);
+			if(value1 == 0){
+				value1 =255;
+			} else {
+				value1=0;
+			}
+	}
+
+//middle horizontal
+	for(var i = 0; i<250; i+=12){
+		fill(value2);
+		noStroke();
+		ellipse(100+i,235,60,60);
+			if(value2 == 0){
+				value2 =255;
+			} else {
+				value2=0;
+			}	
+	}
+
+//right vertical
+	for(var i = 0; i<140; i+=9){
+		fill(value1);
+		noStroke();
+		ellipse(340,237+i,60,60);
+			if(value1 == 0){
+				value1 =255;
+			} else {
+				value1=0;
+			}
+	}
+
+//bottom horizontal
+	for(var i = 0; i<262; i+=12){
+		fill(value2);
+		noStroke();
+		ellipse(85+i,385,65,65);
+			if(value2 == 0){
+				value2 =255;
+			} else {
+				value2=0;
+			}	
+	}
+//End S
+
+//Start T
+//horizontal
+	for(var i = 0; i<244; i+=9){
+		fill(value1);
+		noStroke();
+		ellipse(420+i*1.25,100,45,45);
+			if(value1 == 0){
+				value1 =255;
+			} else {
+				value1=0;
+			}	
+	}
+
+//left vertical
+	for(var i = 0; i<300; i+=9){
+		fill(value1);
+		noStroke();
+		ellipse(573,107+i,60,60);
+			if(value1 == 0){
+				value1 =255;
+			} else {
+				value1=0;
+			}
+	}
+}
